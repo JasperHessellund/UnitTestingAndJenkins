7e8b0091607f9880556f6ddc4ab57256c510b49e
Jasper Hessellund <jasperhessellund@gmail.com>
2019-12-17 15:25:22 +0100

    Test for jenkins

--- src/test/java/SortingTest.java
+++ src/test/java/SortingTest.java
@@ -32,7 +32,7 @@ public class SortingTest {
         return Arrays.asList(new int[][][] {
                 {{51,2,3,4,5500, 10000},{}},//upper boundary value 50 returns empty array
                 {{-1,-500,-1000, 1, 2, 3, 4, 5}, {}}, //lower boundary value 0 -||-
-                {{2,3,1,9,8,2}, {1,2,2,3,8,9}}
+                {{2,3,1,9,8,2}, {1,2,2,3,8,9}} //test for jenkins
         });
 
 
